ebba27843f19454d54784f2c814ec51254c13f47
future <commanda.keen@gmail.com>
2013-01-07 17:58:12 +0100

    Removed superfluous logging output

--- cookbooks/biosphere/recipes/default.rb
+++ cookbooks/biosphere/recipes/default.rb
@@ -1,11 +1,5 @@
 logg(%{Cookbook <b>biosphere</b> started...}) { color :magenta }
 
-logg %{HOME: #{ENV['HOME']}}
-logg %{BIOSPHERE_HOME: #{ENV['BIOSPHERE_HOME']}}
-logg %{BIOSPHERE_SPHERE_PATH: #{ENV['BIOSPHERE_SPHERE_PATH']}}
-logg %{BIOSPHERE_SPHERE_AUGMENTATIONS_PATH: #{ENV['BIOSPHERE_SPHERE_AUGMENTATIONS_PATH']}}
-logg %{BIOSPHERE_CUSTOM_SSH_KEY_NAME: #{ENV['BIOSPHERE_ENV_SSH_KEY_NAME']}}
-
 include_recipe 'biosphere::augmentations'
 include_recipe 'biosphere::homebrew'
 include_recipe 'biosphere::rubies'
