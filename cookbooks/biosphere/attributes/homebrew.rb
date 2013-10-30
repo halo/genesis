7f8c6f4135a1a2e435e84399c3733de12febaf07
future <commanda.keen@gmail.com>
2013-10-30 17:20:41 +0100

    Added passenger

--- cookbooks/biosphere/attributes/homebrew.rb
+++ cookbooks/biosphere/attributes/homebrew.rb
@@ -1,13 +1,15 @@
 default[:biosphere][:homebrew][:root]     = File.join(ENV['BIOSPHERE_SPHERE_PATH'], 'homebrew')
 default[:biosphere][:homebrew][:revision] = '7947b538f0d293d786204a56135a71bce1c2f35d'
 
-default[:biosphere][:homebrew][:formulae] = %w{
+default[:biosphere][:homebrew][:formulae] = ['nginx --with-passenger']
+default[:biosphere][:homebrew][:formulae] += %w{
   autoconf
   git
   rbenv
   ruby-build
   brew-cask
 }
+
 
 if %w{ red blue }.include? ENV['BIOSPHERE_ENV_PROFILE']
   default[:biosphere][:homebrew][:formulae] += %w{
