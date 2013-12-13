eb8bd4839f87a45f639480fdb049203b2e934dc1
future <commanda.keen@gmail.com>
2013-12-13 13:00:16 +0100

    changing a resource id

--- cookbooks/biosphere/recipes/osx.rb
+++ cookbooks/biosphere/recipes/osx.rb
@@ -1,7 +1,4 @@
-# ––––––––––––––––––
-# System Preferences
-# ––––––––––––––––––
 
-bash 'General -> Appearance -> Blue' do
+bash 'System Preferences -> General -> Appearance -> Blue' do
   code %{defaults write NSGlobalDomain AppleAquaColorVariant -int 1}
 end
