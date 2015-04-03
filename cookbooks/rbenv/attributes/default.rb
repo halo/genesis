55c4f8b5f3f606e899efc90f7c2c98ea98891316
halo <github@posteo.org>
2015-04-03 11:05:29 +0200

    Updated ruby 2.1.3 -> 2.2.1

--- cookbooks/rbenv/attributes/default.rb
+++ cookbooks/rbenv/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:biosphere][:rbenv][:root]       = File.join(ENV['BIOSPHERE_SPHERE_PATH'], 'rbenv')
-default[:biosphere][:rbenv][:rubies]     = %w{ 2.1.3 }
+default[:biosphere][:rbenv][:rubies]     = %w{ 2.2.1 }
 
 if %w{ red blue }.include? ENV['BIOSPHERE_ENV_PROFILE']
   default[:biosphere][:rbenv][:rubies] += %w{
