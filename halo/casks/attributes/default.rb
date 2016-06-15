aa6b8def56db905a9b41d2848fc00eee081c62ec
halo <github@posteo.org>
2016-06-15 23:17:27 +0200

    [casks] Added tor and textmate

--- halo/casks/attributes/default.rb
+++ halo/casks/attributes/default.rb
@@ -1,29 +1,33 @@
 default[:cask][:caskroom] = File.join(ENV['BIOSPHERE_SPHERE_PATH'], 'caskroom')
 default[:cask][:applinks] = Home.path.join('Applications')  # File.join(ENV['BIOSPHERE_SPHERE_PATH'], 'caskapps')
 
-default[:cask][:apps] = %w{
+default[:cask][:apps] = %w(
   cyberduck
   disk-inventory-x
   firefox
   google-chrome
   mumble
-}
+  torbrowser
+  textmate
+)
 
-# virtualbox (sudo)
-# textmate (sudo)
-# teamviewer (sudo)
-
-if %w{ orange green blue }.include? ENV['BIOSPHERE_ENV_PROFILE']
-  default[:cask][:apps] += %w{
+if %w(orange green blue).include? ENV['BIOSPHERE_ENV_PROFILE']
+  default[:cask][:apps] += %w(
     flux
     arrsync
     seashore
     quicksilver
-  }
+  )
 end
 
-if %w{ blue }.include? ENV['BIOSPHERE_ENV_PROFILE']
-  default[:cask][:apps] += %w{
+if %w(orange green).include? ENV['BIOSPHERE_ENV_PROFILE']
+  default[:cask][:apps] += %w(
+    audacity
+  )
+end
+
+if %w(blue).include? ENV['BIOSPHERE_ENV_PROFILE']
+  default[:cask][:apps] += %w(
     adium
-  }
+  )
 end
