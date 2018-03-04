b4e981535e0078494ade47d997be3a85d0dc0386
halo <github@posteo.org>
2018-03-04 18:09:57 +0100

    Use Pathname to find home directory

--- halo/rbenv/attributes/default.rb
+++ halo/rbenv/attributes/default.rb
@@ -1,2 +1,2 @@
-default[:biosphere][:rbenv][:root]   = File.join(ENV['HOME'], '.rbenv')
+default[:biosphere][:rbenv][:root]   = Home.path.join('.rbenv').to_s
 default[:biosphere][:rbenv][:rubies] = %w[2.4.1]
