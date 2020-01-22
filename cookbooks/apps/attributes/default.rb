f59083691989e253f9f081af0254afb8158653da
halo <github@posteo.org>
2020-01-22 13:59:49 +0100

    [vscode] add gitlens

--- cookbooks/apps/attributes/default.rb
+++ cookbooks/apps/attributes/default.rb
@@ -1,6 +1,7 @@
 default[:apps][:vscode_extensions] = %w[
   Anjali.clipboard-history
   BriteSnow.vscode-toggle-quotes
+  eamodio.gitlens
   ms-vsliveshare.vsliveshare
   rebornix.ruby
   sianglim.slim
