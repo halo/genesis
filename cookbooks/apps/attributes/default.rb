252c0437a7623d4e085105d782b4448c923b5e85
halo <github@posteo.org>
2020-04-29 16:47:01 +0200

    [vscode] add bracket pair colorizer extension

--- cookbooks/apps/attributes/default.rb
+++ cookbooks/apps/attributes/default.rb
@@ -1,6 +1,7 @@
 default[:apps][:vscode_extensions] = %w[
   Anjali.clipboard-history
   BriteSnow.vscode-toggle-quotes
+  CoenraadS.bracket-pair-colorizer-2
   eamodio.gitlens
   ms-vsliveshare.vsliveshare
   rebornix.ruby
