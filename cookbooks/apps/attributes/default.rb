101a47f915b6dff084ffc5436b7ab8fb7a88ab76
halo <github@posteo.org>
2021-01-26 15:01:03 +0100

    [vscode] Add refresh broser and auto-add-brackets

--- cookbooks/apps/attributes/default.rb
+++ cookbooks/apps/attributes/default.rb
@@ -1,11 +1,19 @@
 default[:apps][:vscode_extensions] = %w[
-  Anjali.clipboard-history
-  BriteSnow.vscode-toggle-quotes
-  CoenraadS.bracket-pair-colorizer-2
+  aeschli.vscode-css-formatter
+  aliariff.auto-add-brackets
+  anjali.clipboard-history
+  britesnow.vscode-toggle-quotes
+  coenraads.bracket-pair-colorizer-2
+  dae.vscode-refresh-browser
+  dotjoshjohnson.xml
   eamodio.gitlens
+  mikeburgh.xml-format
   ms-vsliveshare.vsliveshare
   patbenatar.advanced-new-file
   rebornix.ruby
   sianglim.slim
+  syler.sass-indented
+  viktorzetterstrom.non-breaking-space-highlighter
   wenfang.faster-new
+  wingrunr21.vscode-ruby
 ]
