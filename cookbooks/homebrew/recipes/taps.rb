e7a2e53275887fccd290a443b7623a236a90b3a4
halo <github@posteo.org>
2022-10-29 16:48:45 +0200

    Chef still doesn't want /opt for brew

--- cookbooks/homebrew/recipes/taps.rb
+++ cookbooks/homebrew/recipes/taps.rb
@@ -1,6 +1,21 @@
-node[:homebrew][:taps].each do |tap|
-  homebrew_tap tap do
-    homebrew_path Homebrew.brew_path.to_s
-    only_if { Internet.online? }
+if arm?
+  # See https://github.com/chef/chef/blob/b668235003f7f32b96e1efb2e596cc15b0da78fc/lib/chef/mixin/homebrew_user.rb#L63
+  node[:homebrew][:taps].each do |tap|
+    execute "brew tap #{tap}" do
+      command "#{Homebrew.brew_path} tap #{tap}"
+      environment({ HOME: Home.path.to_s, USER: Console.user })
+      user Console.user
+      group 'staff'
+      only_if { Internet.online? }
+    end
+  end
+
+else
+  node[:homebrew][:taps].each do |tap|
+    homebrew_tap tap do
+      owner Console.user
+      only_if { Internet.online? }
+    end
   end
 end
+
