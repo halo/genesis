0d11f8e9efb194976b7e2bff4c4178839b8fb8fc
halo <github@posteo.org>
2022-10-29 18:12:20 +0200

    Use frum for dotfiles

--- cookbooks/dotfiles/metadata.rb
+++ cookbooks/dotfiles/metadata.rb
@@ -3,3 +3,4 @@ version '1.0.0'
 maintainer 'halo'
 
 depends 'smart'
+depends 'frum'
--- cookbooks/dotfiles/recipes/dotfiles.rb
+++ cookbooks/dotfiles/recipes/dotfiles.rb
@@ -1,6 +1,8 @@
 dotfiles_path = Home.path.join('.dotfiles')
 dotfiles_binary = dotfiles_path.join('bin/dotfiles')
 macos_binary = dotfiles_path.join('macos/bin/macos')
+frum_path = Pathname.new node[:frum][:root]
+frum_executable = Homebrew.bin_path.join('frum')
 
 smart_git dotfiles_path.to_s do
   repository 'https://github.com/halo/dotfiles.git'
@@ -9,17 +11,23 @@ smart_git dotfiles_path.to_s do
 end
 
 execute 'run dotfiles' do
-  command dotfiles_binary.to_s
+  environment(lazy { { frum_ROOT: frum_path.to_s } })
+  command %|eval "$(#{frum_executable} init)"; #{dotfiles_binary}|
   user Console.user
   group 'staff'
+  only_if { frum_executable.executable? }
 end
 
 execute 'run userlevel configuration' do
-  command macos_binary.to_s
+  environment(lazy { { frum_ROOT: frum_path.to_s } })
+  command %|eval "$(#{macos_binary} init)"; #{dotfiles_binary}|
   user Console.user
   group 'staff'
+  only_if { frum_executable.executable? }
 end
 
 execute 'run root configuration' do
-  command "#{macos_binary} --only-sudo"
+  environment(lazy { { frum_ROOT: frum_path.to_s } })
+  command %|eval "$(#{macos_binary} init)"; #{macos_binary} --only-sudo|
+  only_if { frum_executable.executable? }
 end
