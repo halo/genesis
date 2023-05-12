9adb92f5c9b486bb7ad59607a65842aad285fc38
halo <github@posteo.org>
2023-05-12 17:59:25 +0200

    Fix template file for key mapping

--- cookbooks/macos/recipes/keys.rb
+++ cookbooks/macos/recipes/keys.rb
@@ -1,7 +1,7 @@
 directory = Home.path.join('/Library/LaunchDaemons/')
 template_file = directory.join('io.github.halo.genesis.user_key_mapping.plist')
 
-# sudo launchctl load -w /Library/LaunchDaemons/org.custom.keyboard-remap.plist
+# sudo launchctl load -w /Library/LaunchDaemonsio.github.halo.genesis.user_key_mapping.plist
 cookbook_file template_file.to_s do
   owner 'root'
   group 'wheel'
