c63ccc0afac9fbadaaa08b625189bf71c79cbdd0
halo <github@posteo.org>
2024-05-23 20:50:46 +0200

    Use ruby-build directly instead of rbenv

--- cookbooks/rbenv/recipes/default.rb
+++ cookbooks/rbenv/recipes/default.rb
@@ -1,29 +1,29 @@
 Genesis.users do |account|
   next unless account.orange? || account.green?
 
-  rbenv_path = account.paths.home.join('.rbenv')
-  rbenv_executable = Homebrew.bin_path.join('rbenv')
+  rbenv_root = account.paths.home.join('.rbenv')
 
-  directory rbenv_path.to_s do
+  directory rbenv_root.to_s do
     owner account.username
     group 'staff'
   end
 
   # Set global default ruby version
-  file rbenv_path.join('version').to_s do
+  file rbenv_root.join('version').to_s do
     content node[:rbenv][:ruby]
     owner account.username
     group 'staff'
   end
 
   version = node[:rbenv][:ruby]
+  ruby_install_dir = rbenv_root.join('versions', version)
+  build_executable = Homebrew.bin_path.join('ruby-build')
+
   execute "install ruby #{version}" do
-    environment(lazy { { rbenv_ROOT: rbenv_path.to_s } })
-    # For the flags, see https://github.com/rbenv/rbenv/issues/121#issuecomment-1256316458
-    command "#{rbenv_executable} install -s #{version}"
-    creates rbenv_path.join('versions', version, 'bin').to_s
+    command "#{build_executable} #{version} #{ruby_install_dir}"
+    creates rbenv_root.join('versions', version, 'bin').to_s
     user account.username
     group 'staff'
-    only_if { rbenv_executable.executable? }
+    only_if { build_executable.executable? }
   end
 end
