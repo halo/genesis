ecd54f0c5771f2775f772f20486cb46b24f31b30
halo <github@posteo.org>
2024-05-23 17:42:47 +0200

    Ignore existing casks (again)

--- cookbooks/casks/recipes/default.rb
+++ cookbooks/casks/recipes/default.rb
@@ -3,6 +3,6 @@ node[:cask][:apps].each do |app|
     install_cask false # Error: Tapping homebrew/cask is no longer typically necessary. Add --force if you are sure you need it for contributing to Homebrew.
     homebrew_path Homebrew.brew_path.to_s
     only_if { Internet.online? }
-   # ignore_failure true # In case an application was installed via dmg
+    ignore_failure true # In case an application was installed via dmg
   end
 end
