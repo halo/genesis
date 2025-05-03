09a1f60f37b141cd1a44c433aaafc601dd791c04
halo <github@posteo.org>
2025-05-03 21:09:56 +0200

    Try to have ruby before installing gems

--- cookbooks/default/recipes/default.rb
+++ cookbooks/default/recipes/default.rb
@@ -1,11 +1,11 @@
 include_recipe 'apps::xcode_license'
 
 include_recipe 'macos'
+include_recipe 'rbenv'
 include_recipe 'dotfiles'
 include_recipe 'homebrew'
 include_recipe 'apps'
 include_recipe 'casks'
-include_recipe 'rbenv'
 include_recipe 'mas'
 
 # Can only run after ruby was installed
