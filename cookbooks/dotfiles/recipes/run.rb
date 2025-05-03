0afe0442eeeca7150e981fa52ecbed11cf1d91ba
halo <github@posteo.org>
2025-05-03 20:55:40 +0200

    Vendorize bundling for dotfiles

--- cookbooks/dotfiles/recipes/run.rb
+++ cookbooks/dotfiles/recipes/run.rb
@@ -1,17 +1,35 @@
-# To avoid a chicken-egg problem, we run the dotfiles here with chef.
-# Because `ruby` is not yet in the PATH, we cannot run the dotfiles.
-# But the dotfiles give us ruby in the PATH.
-# Only here can we fake the PATH.
+# The end-user cannot run the dotfiles after Genesis, because `ruby` is not in the terminal PATH.
+# It is the dotfiles, that add ruby to the PATH, so we have a chicken-egg problem.
+# Here in chef, knowing the path to ruby, we can run the dotfiles once, to kickstart them.
 
 if Genesis.users.any?(&:opinionated?)
-  Genesis.users do |account|
+  Genesis.users do |account| # rubocop:disable Metrics/BlockLength
     dotfiles_binary = account.paths.dotfiles.join('bin/dotfiles')
+    bundle_config_path = account.paths.dotfiles.join('.bundle')
+    bundle_config_file = bundle_config_path.join('config')
+    vendor_bundle = account.paths.dotfiles.join('vendor/bundle')
     macos_binary = account.paths.dotfiles.join('macos/bin/macos')
-    rbenv_executable = ::Homebrew.bin_path.join('rbenv')
+    rbenv_executable = ::Homebrew.bin_path.join('rbenv') # rubocop:disable Style/RedundantConstantBase
     ruby_version = node[:rbenv][:ruby]
     rbenv_init = %|eval "$(#{rbenv_executable} init - zsh)"; #{rbenv_executable} shell #{ruby_version}|
 
+    directory bundle_config_path.to_s do
+      owner account.username
+      group 'staff'
+      mode '0755'
+    end
+
+    template bundle_config_file.to_s do
+      source 'bundle_config.yml.erb'
+      owner account.username
+      group 'staff'
+      mode '0644'
+      variables(bundle_path: vendor_bundle)
+      action :create
+    end
+
     execute "run #{account.username} dotfiles" do
+      cwd '/tmp' # Avoid trying to create `/Gemfile.lock` when another user.
       environment(lazy { { RBENV_ROOT: account.paths.rbenv.to_s, HOME: account.paths.home.to_s } })
       command %(#{rbenv_init}; #{dotfiles_binary})
       user account.username
@@ -21,7 +39,10 @@ if Genesis.users.any?(&:opinionated?)
 
     if account.username == 'admin'
       execute "run #{account.username} root configuration" do
-        environment(lazy { { RBENV_ROOT: account.paths.rbenv.to_s, HOME: account.paths.home.to_s } })
+        environment(lazy do
+          { RBENV_ROOT: account.paths.rbenv.to_s,
+            HOME: account.paths.home.to_s }
+        end)
         command %(#{rbenv_init}; #{macos_binary} --only-sudo)
         only_if { rbenv_executable.executable? }
       end
